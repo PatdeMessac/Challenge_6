f2415971f23d05baf6714bab6474f1afef0fc017
solenehuault <solene.huault@gmail.com>
2016-06-30 17:03:44 +0200

    js- ajout de commentaires

--- action.js
+++ action.js
@@ -4,16 +4,21 @@ var prix = parseInt(document.getElementById("cout").innerHTML);
 var prix_auto = parseInt(document.getElementById("cout_auto").innerHTML);
 var click;
 
+//fonction clicker basique : ajoute 1 au score quand on clique sur le cookie
 function clicker() {
         compteur += multi;
         document.getElementById("affichage").innerHTML=compteur;
 };
 
+//fonction qui incremente le compteur, baisse le score
 function incrementer() {
+
+	//si on a un score assez élévé
 	if (compteur >= prix) {
 		multi ++;
 		compteur -= prix;
 		prix *= 2;
+		//affiche les changements
 		document.getElementById("affichage").innerHTML=compteur;
 		document.getElementById("multiplicateur").innerHTML=multi;
 		document.getElementById("cout").innerHTML=prix;
@@ -23,11 +28,15 @@ function incrementer() {
 	}
 };
 
+
+//fonction qui appelle la fonction clicker en fonction d'un timer
 function autoclick() {
+	//si on a un score assez élévé
 	if (compteur >= prix_auto) {
 		click = setInterval(clicker, 1000);
 		compteur -= prix_auto;
 		prix_auto *= 2;
+		//affiche les changements
 		document.getElementById("affichage").innerHTML=compteur;
 		document.getElementById("cout_auto").innerHTML=prix_auto;
 	}

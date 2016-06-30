00dd41f5335ed4adc77bd35720b5e5f3d9d00b50
solenehuault <solene.huault@gmail.com>
2016-06-30 13:45:41 +0200

    js- ajout cout du multi a 50

--- action.js
+++ action.js
@@ -9,8 +9,14 @@ function clicker() {
 };
 
 function incrementer() {
-	multi ++;
-	document.getElementById("multiplicateur").innerHTML=multi;
-	
+	if (compteur >= 50) {
+		multi ++;
+		compteur -= 50;
+		element.innerHTML=compteur;
+		document.getElementById("multiplicateur").innerHTML=multi;
+	}
+	else {
+		alert("Vous devez avoir un score d'au moins 50 pour acheter le multiplicateur !!!!");
+	}
 };
 

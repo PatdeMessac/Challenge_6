e40cd68a3549aa197c1c746f7cbecddbdeec8193
solenehuault <solene.huault@gmail.com>
2016-07-11 16:52:55 +0200

    variables renommé anglais

--- action.js
+++ action.js
@@ -1,44 +1,44 @@
-var compteur = parseInt(document.getElementById("affichage").innerHTML);
-var multi = parseInt(document.getElementById("multiplicateur").innerHTML);
-var prix = parseInt(document.getElementById("cout").innerHTML);
-var prix_auto = parseInt(document.getElementById("cout_auto").innerHTML);
-var click;
+var counter = 0;
+var multi = 1;
+var price = 5;
+var price_auto = 20;
 
-//fonction clicker basique : ajoute 1 au score quand on clique sur le cookie
+//fonction clicker basique : ajoute 1 + le multiplicateur au score quand on clique sur le cookie
 function clicker() {
-        compteur += multi;
-        document.getElementById("affichage").innerHTML=compteur;
+        counter += multi;
+        document.getElementById("affichage").innerHTML=counter;
 };
 
 //fonction qui incremente le compteur, baisse le score
 function incrementer() {
 
 	//si on a un score assez élévé
-	if (compteur >= prix) {
+	if (counter >= price) {
 		multi ++;
-		compteur -= prix;
-		prix *= 2;
+		counter -= price;
+		price *= 2;
 		//affiche les changements
-		document.getElementById("affichage").innerHTML=compteur;
+		document.getElementById("affichage").innerHTML=counter;
 		document.getElementById("multiplicateur").innerHTML=multi;
-		document.getElementById("cout").innerHTML=prix;
+		document.getElementById("cout").innerHTML=price;
 	}
 	else {
-		alert("Vous devez avoir un score d'au moins " + prix + " acheter pour le multiplicateur !!!!");
+		alert("Vous devez avoir un score d'au moins " + price + " acheter pour le multiplicateur !!!!");
 	}
 };
 
 
 //fonction qui appelle la fonction clicker en fonction d'un timer
 function autoclick() {
+	var click;	
 	//si on a un score assez élévé
-	if (compteur >= prix_auto) {
+	if (counter >= price_auto) {
 		click = setInterval(clicker, 1000);
-		compteur -= prix_auto;
-		prix_auto *= 2;
+		counter -= prix_auto;
+		price_auto *= 2;
 		//affiche les changements
-		document.getElementById("affichage").innerHTML=compteur;
-		document.getElementById("cout_auto").innerHTML=prix_auto;
+		document.getElementById("affichage").innerHTML=counter;
+		document.getElementById("cout_auto").innerHTML=price_auto;
 	}
 	else {
 		alert("shoo");
